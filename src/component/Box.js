aba7d445f090862c70bcc0462a3f1b88bf828794
minsun283 <10000.hohoho@gmail.com>
2024-08-13 16:32:26 +0900

    Update Box.js

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className='box-deco'>
       <h3>{props.title}</h3>
-      <img className='img-size' src={props.item &&props.item.img}/></img>
+      <img className='img-size' alt="img" src={props.item &&props.item.img}/></img>
       <h3>WIN</h3>
       </div>
   )
